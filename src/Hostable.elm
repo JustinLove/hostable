41e828efd9dbd34840605dc9aef1e62b73a51382
Justin Love <git@JustinLove.name>
2018-03-03 11:28:50 -0600

    extract helix to twitch module

--- src/Hostable.elm
+++ src/Hostable.elm
@@ -4,6 +4,7 @@ import Persist exposing (Persist)
 import Persist.Encode exposing (persist)
 import Persist.Decode exposing (persist)
 import Twitch.Deserialize exposing (User, LiveStream, Game)
+import Twitch exposing (helix)
 import TwitchId
 import UserList
 import View
@@ -266,34 +267,3 @@ fetchGames gameIds =
       , url = (fetchGamesUrl gameIds)
       }
 
-helix :
-  { clientId : String
-  , auth : Maybe String
-  , decoder : Json.Decode.Decoder a
-  , tagger : ((Result Http.Error a) -> Msg)
-  , url : String
-  } -> Cmd Msg
-helix {clientId, auth, decoder, tagger, url} =
-  Http.send tagger <| Http.request
-    { method = "GET"
-    , headers = twitchHeaders clientId auth
-    , url = url
-    , body = Http.emptyBody
-    , expect = Http.expectJson decoder
-    , timeout = Nothing
-    , withCredentials = False
-    }
-
-twitchHeaders : String -> Maybe String -> List Http.Header
-twitchHeaders clientId auth =
-  List.append
-    [ Http.header "Client-ID" clientId
-    ] (authHeaders auth)
-
-authHeaders : Maybe String -> List Http.Header
-authHeaders auth =
-  case auth of
-    Just token ->
-      [ Http.header "Authorization" ("Bearer "++token) ]
-    Nothing ->
-      []
