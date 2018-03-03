0801d060c10258a218a918bf31569b3c5c856c50
Justin Love <git@JustinLove.name>
2018-03-03 10:04:33 -0600

    rearrange screen view to make game and viewers easier to see.

--- src/View.elm
+++ src/View.elm
@@ -14,6 +14,12 @@ type Msg
   = HostClicked String
   | Refresh
 
+boxWidth = 70
+boxHeight = 95
+
+screenWidth = 168
+screenHeight = 95
+
 css = """
 body {
   background-color: rgb(23, 20, 31);
@@ -21,17 +27,20 @@ body {
 }
 #streams { display: flex; flex-wrap: wrap; list-style-type: none;}
 .stream { width: 240px; height: 200px; padding: 10px; }
-.screen { width: 240px; height: 134px; position: relative; }
-.box-art { position: absolute; bottom: 0; right: 0; }
+.graphics { width: 240px; height: 95px; position: relative; }
+.screen { width: 168px; height: 95px; position: absolute; right: 0;}
+.box-art { position: absolute; left: 0; }
 .viewers {
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  color: rgb(218, 216, 222);
-  margin: 0.1em;
+  margin-right: 0.2em;
+  background: rgb(100, 65, 164);
+  color: white;
+  padding: 0.1em;
+  padding-left: 0.3em;
+  padding-right: 0.3em;
 }
 .info { display: flex; overflow-x: hidden; }
 .info-text p { margin: 0.2em; font-size: 0.8em; white-space: nowrap; }
+.game-name { color: rgb(174, 158, 202); }
 .channel {
   background-color: rgb(14, 12, 19);
   color: rgb(250, 249, 250);
@@ -83,23 +92,28 @@ streamView model stream =
     game = gameFor model.games stream
   in
   li [ class "stream" ]
-    [ a [ href ("https://twitch.tv/"++name) ]
-      [ div [ class "screen" ]
-        [ img [ class "preview", src ((imageTemplateUrl 320 180 stream.thumbnailUrl) ++ "?" ++ (toString model.previewVersion)), width 239, height 134 ] []
-        , displayBoxArt game
-        , p [ class "viewers" ] [ text <| toString stream.viewerCount ]
+    [ div [ class "graphics" ]
+      [ displayBoxArt game
+      , a [ href ("https://twitch.tv/"++name) ]
+        [ div [ class "screen" ]
+          [ img [ class "preview", src ((imageTemplateUrl screenWidth screenHeight stream.thumbnailUrl) ++ "?" ++ (toString model.previewVersion)), width screenWidth, height screenHeight ] []
+          ]
         ]
       ]
     , div [ class "info" ]
       [ div [ class "info-text" ]
-        [ p [ class "title", title stream.title ] [ text stream.title]
-        , input
-          [ class "channel"
-          , id ("host-" ++ name)
-          , Html.Events.onClick (HostClicked ("host-" ++ name))
-          , readonly True
-          , value ("/host " ++ name)
-          ] []
+        [ div [ class "viewers-channel" ]
+          [ span [ class "viewers" ] [ text <| toString stream.viewerCount ]
+          , input
+            [ class "channel"
+            , id ("host-" ++ name)
+            , Html.Events.onClick (HostClicked ("host-" ++ name))
+            , readonly True
+            , value ("/host " ++ name)
+            ] []
+          ]
+        , let gameName = nameOfGame game in p [ class "game-name", title gameName] [ text gameName ]
+        , p [ class "title", title stream.title ] [ text stream.title]
         , ul [ class "comments" ]
           (List.map (li [] << List.singleton << text)
             <| commentsForStream name UserList.users)
@@ -118,22 +132,31 @@ gameFor games stream =
   List.filterMap (\g -> if g.id == stream.gameId then Just g else Nothing) games
    |> List.head
 
+
 displayBoxArt : Maybe Game -> Html Msg
 displayBoxArt mgame =
   case mgame of
     Just game ->
       img
         [ class "box-art"
-        , src <| imageTemplateUrl 38 52 game.boxArtUrl
-        , width 38
-        , height 52
+        , src <| imageTemplateUrl boxWidth boxHeight game.boxArtUrl
+        , width boxWidth
+        , height boxHeight
         , title game.name
         ] []
     Nothing ->
       div
         [ class "box-art"
-        , style [ ("width", "38px"), ("height", "52px") ]
+        , style [ ("width", (toString boxWidth) ++ "px"), ("height", (toString boxHeight) ++ "px") ]
         ] []
+
+nameOfGame : Maybe Game -> String
+nameOfGame mgame =
+  case mgame of
+    Just game ->
+      game.name
+    Nothing ->
+      "--"
 
 commentsForStream : String -> List (String, List String) -> List String
 commentsForStream userName users =
