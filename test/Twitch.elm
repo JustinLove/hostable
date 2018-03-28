7cdf6adc8ed76a7eff481c97ce2ca0cc5f57500a
Justin Love <git@JustinLove.name>
2018-03-28 12:27:22 -0500

    simplify test to only that decode succeeds

--- test/Twitch.elm
+++ test/Twitch.elm
@@ -14,25 +14,11 @@ main =
 all : Test
 all = describe "Deserialize"
   [ it "deserializes sample user" <|
-    eql
-      (Json.Decode.decodeString users sampleUser)
-      (Ok [
-        ( User
-          "44322889"
-          "dallas"
-          "dallas"
-          "staff"
-          ""
-          "Just a gamer playing games and chatting. :)"
-          "https://static-cdn.jtvnw.net/jtv_user_pictures/dallas-profile_image-1a2c906ee2c35f12-300x300.png"
-          "https://static-cdn.jtvnw.net/jtv_user_pictures/dallas-channel_offline_image-1a2c906ee2c35f12-1920x1080.png"
-          191836881
-          (Just "login@provider.com")
-        )])
+    decodes <| Json.Decode.decodeString users sampleUser
   ]
 
-roundTrips : (a -> Json.Decode.Value) -> Json.Decode.Decoder a -> a -> Expectation.Expectation
-roundTrips encoder decoder value =
-  eql
-    (Ok value)
-    (value |> encoder |> Json.Decode.decodeValue decoder)
+decodes : Result String a -> Expectation.Expectation
+decodes result =
+  isTrue (case result of
+    Ok _ -> True
+    Err _ -> False)
