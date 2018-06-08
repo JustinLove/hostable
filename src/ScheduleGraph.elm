c42e31069cdf3a81d042732bd6c75a067df24efd
Justin Love <git@JustinLove.name>
2018-06-08 09:19:36 -0500

    import changes to schedule graph - bars, time range

--- src/ScheduleGraph.elm
+++ src/ScheduleGraph.elm
@@ -8,7 +8,7 @@ import Collage.Render
 import Color exposing (Color)
 import Svg
 import Svg.Attributes
-import Date exposing (Day(..))
+import Date exposing (Day(..), Month(..))
 import Time exposing (Time)
 
 type alias ScheduleGraph =
@@ -36,6 +36,35 @@ type alias Style =
 
 day = toFloat (24 * 60 * 60 * 1000)
 allDays = [Mon, Tue, Wed, Thu, Fri, Sat, Sun]
+dayStripe dow =
+  case dow of
+    Mon -> False
+    Tue -> True
+    Wed -> False
+    Thu -> True
+    Fri -> False
+    Sat -> True
+    Sun -> False
+monthNum mon =
+  case mon of
+    Jan -> "01"
+    Feb -> "02"
+    Mar -> "03"
+    Apr -> "04"
+    May -> "05"
+    Jun -> "06"
+    Jul -> "07"
+    Aug -> "08"
+    Sep -> "09"
+    Oct -> "10"
+    Nov -> "11"
+    Dec -> "12"
+numberPad : Int -> String
+numberPad i =
+  if i < 10 then
+    "0" ++ (toString i)
+  else
+    toString i
 
 scheduleGraph : List (Svg.Attribute msg) -> ScheduleGraph -> Html msg
 scheduleGraph attributes {width, height, time, events, days, style} =
@@ -43,17 +72,20 @@ scheduleGraph attributes {width, height, time, events, days, style} =
     even = height / (toFloat (List.length days) + 1)
     axis = min even (min (height/4) (width / 15))
     row = (height - axis) / (toFloat (List.length days))
+    starts = List.map .start events
+    first = List.minimum starts |> Maybe.withDefault 0
+    timeRange = (first, (List.maximum starts |> Maybe.withDefault 0) - first)
   in
     [ days 
       |> List.map (eventsOnDay <| breakOverDays events)
-      |> List.map ((rowHeatMap style)
+      |> List.map ((rowHeatMap style timeRange)
         >> scaleX width
         >> scaleY row
         )
       |> List.map2 (contextDecorations style time) days
       |> vertical
       |> align top
-    , displayScale style width height axis
+    , displayScale timeRange style width height axis
       |> align top
     ]
       |> stack
@@ -92,15 +124,17 @@ eventsOnDay : List Event -> Day -> List Event
 eventsOnDay events dow =
   List.filter (\vid -> (dayOfWeek vid.start) == dow) events
 
-rowHeatMap : Style -> List Event -> Collage msg
-rowHeatMap style events =
+rowHeatMap : Style -> (Time, Time) -> List Event -> Collage msg
+rowHeatMap style ((first, range) as timeRange) events =
   events
-    |> toRanges
-    |> List.map (\(start, duration) ->
-      rectangle (duration / day) 1
+    |> toRanges timeRange
+    |> List.map (\(start, duration, age) ->
+      rectangle (duration / day) 0.1
         |> filled (uniform style.dataColor)
-        |> opacity 0.3
+        |> opacity 0.9
         |> shiftX (((start + duration/2) / day) - 0.5)
+        |> shiftY ((age / range) - 0.5)
+        |> scaleY 0.70
       )
     |> group
     |> setEnvelope 1 1
@@ -112,7 +146,13 @@ contextDecorations style time dow collage =
     height = Layout.height collage
     mark = max 1 (logBase 10 width)
   in
-  [ (segment (0, 0.51 * height) (0, -0.51 * height)
+  [ if dayStripe dow then
+      rectangle width height
+        |> filled (uniform style.labelColor)
+        |> opacity 0.1
+    else
+      Layout.empty
+  , (segment (0, 0.51 * height) (0, -0.51 * height)
       |> traced (solid mark (uniform style.currentTimeColor))
       |> shiftX (((offset time) / day - 0.5) * width)
       )
@@ -134,8 +174,35 @@ contextDecorations style time dow collage =
   ]
   |> group
 
-displayScale : Style -> Float -> Float -> Float -> Collage msg
-displayScale style width height line =
+displayScale : (Time, Time) -> Style -> Float -> Float -> Float -> Collage msg
+displayScale (first, range) style width height line =
+  let
+    startDate = Date.fromTime first
+    start = (toString <| Date.year startDate) ++ "-" ++
+            (monthNum <| Date.month startDate) ++ "-" ++
+            (numberPad <| Date.day startDate)
+    endDate = Date.fromTime (first + range)
+    end = (toString <| Date.year endDate) ++ "-" ++
+          (monthNum <| Date.month endDate) ++ "-" ++
+          (numberPad <| Date.day endDate)
+  in
+  [ displayTimeScale style width height line
+  , (fromString start)
+    |> Text.size (round <| line/4)
+    |> Text.color style.labelColor
+    |> rendered
+    |> shiftY (height - line/4)
+    |> shiftX (-width/4)
+  , (fromString end)
+    |> Text.size (round <| line/4)
+    |> Text.color style.labelColor
+    |> rendered
+    |> shiftY (line * 1.1)
+    |> shiftX (width * 0.4)
+  ] |> group
+
+displayTimeScale : Style -> Float -> Float -> Float -> Collage msg
+displayTimeScale style width height line =
   let mark = max ultrathin ((logBase 10 width) * 0.55) in
   [0, 3, 6, 9, 12, 15, 18, 21, 24]
     |> List.map (\hour ->
@@ -153,14 +220,15 @@ displayScale style width height line =
     )
     |> group
 
-toRanges : List Event -> List (Time, Time)
-toRanges =
-  List.map toRange
+toRanges : (Time, Time) -> List Event -> List (Time, Time, Time)
+toRanges timeRange =
+  List.map (toRange timeRange)
 
-toRange : Event -> (Time, Time)
-toRange event =
+toRange : (Time, Time) -> Event -> (Time, Time, Time)
+toRange (first, range) event =
   ( offset event.start
   , event.duration
+  , first + range - event.start
   )
 
 dayOfWeek : Time -> Day
